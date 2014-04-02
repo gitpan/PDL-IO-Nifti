2b2189c6b02d1381ea9de496913a501d8e80f708
Ingo Schmid <fantasma@cpan.org>
2014-04-02 08:56:07 +0000

    Import of FANTASMA/PDL-IO-Nifti-0.73 from CPAN.
    gitpan-cpan-distribution: PDL-IO-Nifti
    gitpan-cpan-version:      0.73
    gitpan-cpan-path:         FANTASMA/PDL-IO-Nifti/PDL-IO-Nifti-0.73.tar.gz
    gitpan-cpan-author:       FANTASMA
    gitpan-cpan-maturity:     released

--- lib/PDL/IO/Nifti.pm
+++ lib/PDL/IO/Nifti.pm
@@ -19,7 +19,7 @@ use strict;
 my $template='ic10c18isCCs8fffssssf8fffsccffffiic80c24ssfffffff4f4f4c16c4'; #see nifti1.h
 
 my $byte_order='';
-our $VERSION='0.72';
+our $VERSION='0.73';
 # define hash;
 my %sizes=(
 	'c'=>1,
